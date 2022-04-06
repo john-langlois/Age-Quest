ef09a3c2e85858fffe5511adec34cad14f0d617f
abdulrhamansaleh <asaleh64@uwo.ca>
2022-04-06 01:11:47 -0400

    main men finalized

--- Age-Quest/Assets/__Scripts/MainMenu.cs
+++ Age-Quest/Assets/__Scripts/MainMenu.cs
@@ -10,4 +10,13 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);// this can load the scene number 1 (game)//SceneManager.GetActiveScene().buildIndex +1
     }
+    public void paula()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);// this can load the scene number 1 (game)//SceneManager.GetActiveScene().buildIndex +1
+    }
+     public void dacota()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);// this can load the scene number 1 (game)//SceneManager.GetActiveScene().buildIndex +1
+    }
+    
 }
